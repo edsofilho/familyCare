61740028ee8199430825a675f43d04431186127c
Pc-Lab <ds-mtec-3@servidor.acad>
2025-06-06 14:00:26 -0300

    update

---
+++ Web/fapp.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+feedbacks = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+
+        feedbacks.append({
+            'nome': nome,
+            'email': email,
+            'assunto': assunto,
+            'mensagem': mensagem
+        })
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html', feedbacks=feedbacks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
